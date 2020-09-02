36bd1e4973b2c7d14bbe4038f259d00d09c3d6a1
Spencer Kasper <spencer.kasper@gmail.com>
2020-09-01 22:46:29 -0500

    Revert "* Add a couple of routes that remind me how to do query params and route params"
    This reverts commit 1ed9dab0

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import {defaultRoute, withParametersRoute, withQueryRoute} from "./routes/helloWorldService";
+import {defaultRoute} from "./routes/helloWorldService";
 
 const PORT = 3000;
 const app = express();
 
 app.get('/', defaultRoute);
-
-app.get('/withParameters/:withParameters', withParametersRoute);
-
-app.get('/withQuery', withQueryRoute);
 
 app.listen(PORT, () => {
     console.log(`Hello world at http://localhost:${PORT}!`)
--- src/routes/helloWorldService.ts
+++ src/routes/helloWorldService.ts
@@ -1,14 +1,3 @@
 export const defaultRoute = (request, response) => {
     response.send('Hello World!')
 };
-
-export const withParametersRoute = (request, response) => {
-    const {withParameters} = request.params;
-    response.send(withParameters);
-};
-
-export const withQueryRoute = (request, response) => {
-    const {queryParam} = request.query;
-
-    response.send(queryParam);
-};
